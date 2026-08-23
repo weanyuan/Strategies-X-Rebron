94d1ae6eb182479268a9c4086194221a4d28520b
weanyuan <124502995+weanyuan@users.noreply.github.com>
2026-08-23 16:22:40 +0800

    1

--- TDS/MainSource.lua
+++ TDS/MainSource.lua
@@ -569,52 +569,6 @@ if CheckPlace() then
 			TimerCheck = false
 		end
 	end)
-	-- // AutoSkip & Auto Start Game
-	local VoteGUI = LocalPlayer.PlayerGui:WaitForChild("ReactOverridesVote"):WaitForChild("Frame"):WaitForChild("votes"):WaitForChild("container",15)
-	if VoteGUI:WaitForChild("prompt").Text == "Ready?" then --Event GameMode
-		task.spawn(function()
-			repeat task.wait() until StratXLibrary.Executed
-			RemoteFunction:InvokeServer("Voting", "Skip")
-			prints("Ready Signal Fired")
-		end)
-	end
-	StratXLibrary.ReadyState = false
-	StratXLibrary.VoteState = VoteGUI:GetPropertyChangedSignal("Position"):Connect(function()
-		if VoteGUI:WaitForChild("count").Text ~= `0/{#Players:GetChildren()} Required` then
-			repeat
-				task.wait()
-			until VoteGUI:WaitForChild("count").Text == `0/{#Players:GetChildren()} Required`
-		end
-		if VoteGUI.Position ~= UDim2.new(0.5, 0, 0.5, 0) then --UDim2.new(scale_x, offset_x, scale_y, offset_y)
-			return
-		end
-		local currentPrompt = VoteGUI:WaitForChild("prompt").Text
-		   if currentPrompt == "Ready?" or currentPrompt == "Skip Cutscene?" then --Event GameMode
-		task.wait(0.5)
-			   RemoteFunction:InvokeServer("Voting", "Skip")
-			   StratXLibrary.ReadyState = true
-			if currentPrompt == "Ready?" then
-				prints("Ready Signal Fired")
-			elseif currentPrompt == "Skip Cutscene?" then
-				prints("Skipped Cutscene")
-			end
-			   return
-		   end
-		   if not UtilitiesConfig.AutoSkip then
-			   repeat
-				   task.wait()
-				   if VoteGUI:WaitForChild("count").Text ~= `0/{#Players:GetChildren()} Required` then
-					   return
-				   end
-			   until UtilitiesConfig.AutoSkip
-		   end
-		   if currentPrompt == "Skip Wave?" then
-			   RemoteFunction:InvokeServer("Voting", "Skip")
-			   SetActionInfo("Skip","Total")
-			   SetActionInfo("Skip")
-			   ConsoleInfo(`Skipped Wave {tonumber(GameWave.Text)}`)
-		   end
-	end)
 
 	-- // Platform Stand InGame
 	task.spawn(function()
@@ -1279,6 +1233,54 @@ Functions.MatchMaking = function()
 	task.wait(6)
 	StratXLibrary.Strat.ChosenID = Index
 	ConsoleInfo(`Map Selected: {MapProps.Map}, Mode: {MapProps.Mode}, Solo Only: {MapProps.Solo}`)
+
+    -- // AutoSkip & Auto Start Game
+	local VoteGUI = LocalPlayer.PlayerGui:WaitForChild("ReactOverridesVote"):WaitForChild("Frame"):WaitForChild("votes"):WaitForChild("container")
+	if VoteGUI:WaitForChild("prompt").Text == "Ready?" then --Event GameMode
+		task.spawn(function()
+			repeat task.wait() until StratXLibrary.Executed
+			RemoteFunction:InvokeServer("Voting", "Skip")
+			prints("Ready Signal Fired")
+		end)
+	end
+	StratXLibrary.ReadyState = false
+	StratXLibrary.VoteState = VoteGUI:GetPropertyChangedSignal("Position"):Connect(function()
+		if VoteGUI:WaitForChild("count").Text ~= `0/{#Players:GetChildren()} Required` then
+			repeat
+				task.wait()
+			until VoteGUI:WaitForChild("count").Text == `0/{#Players:GetChildren()} Required`
+		end
+		if VoteGUI.Position ~= UDim2.new(0.5, 0, 0.5, 0) then --UDim2.new(scale_x, offset_x, scale_y, offset_y)
+			return
+		end
+		local currentPrompt = VoteGUI:WaitForChild("prompt").Text
+		   if currentPrompt == "Ready?" or currentPrompt == "Skip Cutscene?" then --Event GameMode
+		task.wait(0.5)
+			   RemoteFunction:InvokeServer("Voting", "Skip")
+			   StratXLibrary.ReadyState = true
+			if currentPrompt == "Ready?" then
+				prints("Ready Signal Fired")
+			elseif currentPrompt == "Skip Cutscene?" then
+				prints("Skipped Cutscene")
+			end
+			   return
+		   end
+		   if not UtilitiesConfig.AutoSkip then
+			   repeat
+				   task.wait()
+				   if VoteGUI:WaitForChild("count").Text ~= `0/{#Players:GetChildren()} Required` then
+					   return
+				   end
+			   until UtilitiesConfig.AutoSkip
+		   end
+		   if currentPrompt == "Skip Wave?" then
+			   RemoteFunction:InvokeServer("Voting", "Skip")
+			   SetActionInfo("Skip","Total")
+			   SetActionInfo("Skip")
+			   ConsoleInfo(`Skipped Wave {tonumber(GameWave.Text)}`)
+		   end
+	end)
+
 end
 
 --Side modes that aren't main ones
